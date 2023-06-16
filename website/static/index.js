763a974cdf6d5d7c293f38c1652201b37987a6c8
AlexValdez85 <alejandrovaldez802@gmail.com>
2023-06-16 11:08:04 -0600

    cambios en la estetica

--- website/static/index.js
+++ website/static/index.js
@@ -1,3 +1,19 @@
+const temaOscuro = () => {
+  document.querySelector("body").setAttribute("data-bs-theme", "dark");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+}
+
+const temaClaro = () => {
+  document.querySelector("body").setAttribute("data-bs-theme", "light");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+}
+
+const cambiartema = () => {
+  document.querySelector("body").setAttribute("data-bs-theme") === "light"?
+  temaOscuro() : temaClaro();
+
+}
+
 function deleteNote(noteId) {
     fetch("/delete-note", {
       method: "POST",
@@ -6,3 +22,4 @@ function deleteNote(noteId) {
       window.location.href = "/";
     });
   }
+
